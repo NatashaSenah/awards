8100d1caa70c4cd20d7e93ab5fcda2f904343f79
NatashaSenah <tashsenah@gmail.com>
2018-10-12 14:28:27 +0300

    add the modified awards

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_image = models.ImageField(upload_to = 'image/')
+    project_title = models.TextField()
+    project_description = models.TextField()
+    project_link = models.TextField()
+class Profile(models.Model):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_photo= models.ImageField()
+    
+
+    def __str__(self):
+    
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    class Meta:
+        ordering = ['bio']
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = cls.objects.filter(user__username__icontains=name)
+        return profile
+
+
+    
