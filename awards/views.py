47cfbc9cca35cd999bfa6831274168d2807a0a46
NatashaSenah <tashsenah@gmail.com>
2018-10-13 12:25:10 +0300

    add the login form

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required.
 from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
 def home(request):
      return render(request, 'awards.html')
+@login_required(login_url='/accounts/login/')
+def article(request, article_id):
