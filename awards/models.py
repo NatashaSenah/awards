763fe4e568eb85d10258886135f06c3b9bb0edeb
NatashaSenah <tashsenah@gmail.com>
2018-10-15 10:56:06 +0300

    add the modified awards

--- awards/models.py
+++ awards/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 # Create your models here.
 class Project(models.Model):
     project_image = models.ImageField(upload_to = 'image/')
     project_title = models.TextField()
     project_description = models.TextField()
     project_link = models.TextField()
+    post = HTMLField()
+    phone_number = models.CharField(max_length = 10)
+    @classmethod
+    def search_by_title(cls,search_term):
+        awards = cls.objects.filter(title__icontains=search_term)
+        return awards
+    #  def __str__(self):
+    #     return self.first_name
+
+    def save_project(self):
+        self.save()
 class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     profile_photo= models.ImageField()
